3ff6208bc727d3fd3e2ffa34ee65ea06b5253906
Evgenii Puliaev <puliaevevgenii@gmail.com>
2020-06-22 21:27:13 +0300

    init commit

---
+++ src/main/kotlin/com/progeri/healthymarket/HealthyMarketApplication.kt
@@ -0,0 +1,11 @@
+package com.progeri.healthymarket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HealthyMarketApplication
+
+fun main(args: Array<String>) {
+    runApplication<HealthyMarketApplication>(*args)
+}
