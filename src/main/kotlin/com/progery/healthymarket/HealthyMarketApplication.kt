9423e702f88db17ff3196379f5fe3d30a74a133a
Evgenii Puliaev <puliaevevgenii@gmail.com>
2020-06-22 23:25:38 +0300

    package name changed

--- src/main/kotlin/com/progery/healthymarket/HealthyMarketApplication.kt
+++ src/main/kotlin/com/progery/healthymarket/HealthyMarketApplication.kt
@@ -1,4 +1,4 @@
-package com.progeri.healthymarket
+package com.progery.healthymarket
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- src/test/kotlin/com/progery/healthymarket/HealthyMarketApplicationTests.kt
+++ src/test/kotlin/com/progery/healthymarket/HealthyMarketApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.progeri.healthymarket
+package com.progery.healthymarket
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
